98298b0268077417f64e15b48472c68495668711
Sebastien Bourdeauducq <sb@m-labs.hk>
2014-02-16 15:24:04 +0100

    Base SoC and software

---
+++ software/main.c
@@ -0,0 +1,71 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <irq.h>
+#include <uart.h>
+#include <generated/csr.h>
+#include <console.h>
+
+static char *readstr(void)
+{
+	char c[2];
+	static char s[64];
+	static int ptr = 0;
+
+	if(readchar_nonblock()) {
+		c[0] = readchar();
+		c[1] = 0;
+		switch(c[0]) {
+			case 0x7f:
+			case 0x08:
+				if(ptr > 0) {
+					ptr--;
+					putsnonl("\x08 \x08");
+				}
+				break;
+			case 0x07:
+				break;
+			case '\r':
+			case '\n':
+				s[ptr] = 0x00;
+				putsnonl("\n");
+				ptr = 0;
+				return s;
+			default:
+				if(ptr >= (sizeof(s) - 1))
+					break;
+				putsnonl(c);
+				s[ptr] = c[0];
+				ptr++;
+				break;
+		}
+	}
+	return NULL;
+}
+
+static void console_service(void)
+{
+	char *str;
+
+	str = readstr();
+	if(str == NULL) return;
+
+	if(strcmp(str, "reboot") == 0) asm("call r0");
+	else if (strcmp(str, "test") == 0) puts("Hello World\n");
+}
+
+int main(void)
+{
+	irq_setmask(0);
+	irq_setie(1);
+	uart_init();
+	
+	puts("DDS/TTL test software built "__DATE__" "__TIME__"\n");
+		
+	while(1) {
+		console_service();
+	}
+	
+	return 0;
+}
---
+++ targets/ddsttltester.py
@@ -0,0 +1,33 @@
+from migen.fhdl.std import *
+from migen.bus import wishbone
+
+from misoclib import gpio, spiflash
+from misoclib.gensoc import GenSoC
+
+class DDSTTLTesterSoC(GenSoC):
+	default_platform = "papilio_pro"
+
+	def __init__(self, platform):
+		GenSoC.__init__(self, platform,
+			clk_freq=32*1000000,
+			cpu_reset_address=0x60000)
+
+		# We can't use reset_less as LM32 does require a reset signal
+		self.clock_domains.cd_sys = ClockDomain()
+		self.comb += self.cd_sys.clk.eq(platform.request("clk32"))
+		self.specials += Instance("FD", p_INIT=1, i_D=0, o_Q=self.cd_sys.rst, i_C=ClockSignal())
+
+		# BIOS is in SPI flash
+		self.submodules.spiflash = spiflash.SpiFlash(platform.request("spiflash2x"),
+			cmd=0xefef, cmd_width=16, addr_width=24, dummy=4)
+		self.register_rom(self.spiflash.bus)
+
+		# Use block RAM instead of SDRAM for now
+		sys_ram_size = 32*1024
+		self.submodules.sys_ram = wishbone.SRAM(sys_ram_size)
+		self.add_wb_slave(lambda a: a[27:29] == 2, self.sys_ram.bus)
+		self.add_cpu_memory_region("sdram", 0x40000000, sys_ram_size)
+
+		self.submodules.leds = gpio.GPIOOut(platform.request("user_led"))
+
+default_subtarget = DDSTTLTesterSoC
